69692b7dac35734cd5d8ca23a56dfe9b4cb8c4d1
LakshAditya <lakshaditys18@gmail.com>
2025-09-26 11:25:06 +0530

    made changes to ocr

--- backend/app/ocr.py
+++ backend/app/ocr.py
@@ -1,32 +1,46 @@
 import easyocr
 import numpy as np
 import fitz
+import json
+
 
 class OCR_Manager:
-    def read_data(pdf_path:str) -> list[dict]:
+    def __init__(self, pdf_path:str):
+        self.__reader = easyocr.Reader(['en']) 
+        self.__document = fitz.open(pdf_path)
+            
+    def process_doc(self, output_dir:str|None = None) -> None:
+        try:
+            for i, page in enumerate(self.__document):
+                print(f"Performing OCR on PAGE {i+1}.")
+                page_pix = page.get_pixmap(dpi=300)
+                image_vector = np.frombuffer(page_pix.samples, dtype=np.uint8).reshape(page_pix.height, page_pix.width, page_pix.n)
+                page_json = self.__process_page(image_vector, i)
+                if output_dir:
+                    
+                    with open(f"{output_dir}/{i+1}.json", "w") as f:
+                        f.write(json.dumps(page_json, indent=1))
+                        f.close()
+            self.__document.close()
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def __process_page(self, image_vector, page_number:int) -> list[dict]:
         try:
             data = {
                 "bounding-boxes":[],
                 "content":[],
-                "conf-score":[]
-            }
-            reader = easyocr.Reader(['en']) 
-            document = fitz.open(pdf_path)
-            for i, page in enumerate(document):
-                print(f"Performing OCR on PAGE {i+1}.")
-                page_pix = page.get_pixmap(dpi=300)
-                image_vector = np.frombuffer(page_pix.samples, dtype=np.uint8).reshape(page_pix.height, page_pix.width, page_pix.n)
-                results = reader.readtext(image_vector)
-                for sentence in results:
-                    data["bounding-boxes"].append(sentence[0])
-                    data["content"].append(sentence[1])
-                    data["conf-score"].append(sentence[2])
-            document.close()
+                "conf-score":[],
+                "page": page_number + 1
+            } 
+            results = self.__reader.readtext(image_vector)
+            for sentence in results:
+                for i in range(4):
+                    for j in range(2):
+                        sentence[0][i][j] = float(sentence[0][i][j])
+                data["bounding-boxes"].append(sentence[0])
+                data["content"].append(sentence[1])
+                data["conf-score"].append(float(sentence[2]))
             return data
         except Exception as e:
             print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    ocm = OCR_Manager()
-    data = ocm.read_data("test/pdfs/ML_Assignment.pdf")
-    print(" ".join(data["content"]))
