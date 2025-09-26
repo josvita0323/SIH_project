02e6530fdc626037337e4e205ae8014b7a2a8767
LakshAditya <lakshaditys18@gmail.com>
2025-09-26 21:28:25 +0530

    changes to ocr

--- backend/app/ocr.py
+++ backend/app/ocr.py
@@ -1,12 +1,11 @@
-import easyocr
+import pytesseract as pyt
 import numpy as np
 import fitz
 import json
 
 
 class OCR_Manager:
-    def __init__(self, pdf_path:str):
-        self.__reader = easyocr.Reader(['en']) 
+    def __init__(self, pdf_path:str): 
         self.__document = fitz.open(pdf_path)
             
     def process_doc(self) -> list[dict]:
@@ -25,19 +24,12 @@ class OCR_Manager:
     def __process_page(self, image_vector, page_number:int) -> list[dict]:
         try:
             data = {
-                "bounding-boxes":[],
                 "content":[],
-                "conf-score":[],
                 "page-number": page_number + 1
             } 
-            results = self.__reader.readtext(image_vector)
+            results = pyt.image_to_string(image_vector)
             for sentence in results:
-                for i in range(4):
-                    for j in range(2):
-                        sentence[0][i][j] = float(sentence[0][i][j])
-                data["bounding-boxes"].append(sentence[0])
                 data["content"].append(sentence[1])
-                data["conf-score"].append(float(sentence[2]))
             return data
         except Exception as e:
             print(f"An error occurred: {e}")
