232c904d4af96c9cbe262cfaeca8c1c67a5166e9
LakshAditya <lakshaditys18@gmail.com>
2025-09-26 17:44:01 +0530

    minor changes

--- backend/app/test.py
+++ backend/app/test.py
@@ -1,5 +1,6 @@
-from app.crud import upsert_user
+from app.crud import *
 from app.database import create_db_and_tables, get_session
+from app.ocr import OCR_Manager
 
 create_db_and_tables()
 
@@ -7,7 +8,17 @@ create_db_and_tables()
 def main():
     session = next(get_session())
     user = upsert_user(session, "abc@123", "Hii")
-    print("added")
-
+    print(f"User Added - {user}")
+    create_job(session, 1)
+    upsert_upload(session, 1, "test_data/fake_kochi.pdf")
+    upload_path = session.exec(select(Upload.file_path).where(Upload.job_id == 1)).first()
+    ocm = OCR_Manager(upload_path)
+    pages = ocm.process_doc()
+    for page in pages:
+        extrcontent = upsert_extracted_content(session, 1, " ".join(page["content"]), page["page-number"])
+        print(f"{extrcontent.id} has been inserted!")
+        stmt = select(ExtractedContent)
+        output = session.exec(stmt).all()
+        print(output)
 
 main()
